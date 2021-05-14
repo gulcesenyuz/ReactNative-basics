7fbb2399b94cc20563891c36246ea3aeb59db007
gülce <gizemgulce@gmail.com>
2021-05-14 15:34:05 +0300

    text input

--- basics/TextInput.js
+++ basics/TextInput.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextInput, StyleSheet, Text, View } from 'react-native';
 
 // useState-> State Hooks - StatefullWidget
 //const [outputText, setOutputText] = useState('Open up App.js to start working on the project')
 
 
-export default function TextInput() {
+export default function App() {
+    const [enteredGoal, setEnteredGoal] = useState('');
+    const [goals, setGoals] = useState([]);
+
+    function goalInputHandler(enteredText) {
+        setEnteredGoal(enteredText);
+    }
+
+    const addGoalHAndler = () => {
+        // ... -> take existing array
+        setGoals(currentGoals => [...currentGoals, enteredGoal]);
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.inputBox}>
-                <TextInput placeholder="New Goal" style={styles.inputText} />
-                <Button title="ADD" />
+                <TextInput
+                    placeholder="New Goal"
+                    style={styles.inputText}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal}
+                />
+                <Button title="ADD" onPress={addGoalHAndler} />
             </View>
             <View>
 
